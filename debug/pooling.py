ab9a14f1abcd3ad89bed0345fdbdf0cd5e961e3f
kukarek <mik16gt@gmail.com>
2023-10-12 01:27:23 +0300

    start

---
+++ debug/pooling.py
@@ -0,0 +1,246 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import Message
+from aiogram.utils import executor
+import threading
+import image_handler
+from io import BytesIO
+from aiogram.types import InputMediaPhoto
+from PIL import Image
+import os
+import time
+import asyncio
+import subprocess
+import sys
+import states
+
+API_TOKEN = '6588918438:AAEuWOePbDIWlDufBsnHTku9wj9oHlU5IrQ' #тестбот
+
+admins = [1020541698, 6108609160]
+
+#on_off = "Off"
+footage = "Off"
+#условный текстовый массив
+threads = []
+
+# Установка уровня логирования
+logging.basicConfig(level=logging.INFO)
+
+# Инициализация бота и диспетчера
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+def admin(id):
+    for admin in admins:
+        if admin == id:
+            return True
+    return False
+
+@dp.message_handler(commands=['start'])
+async def on_start(message: Message):
+    """
+    Обработчик команды /start.
+    Отправляет приветственное сообщение.
+    """
+    if on_off == "On" or admin(message.from_id):
+
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+        # Добавляем кнопки
+        button1 = types.KeyboardButton("Получить фото (Франция)")
+        button2 = types.KeyboardButton("Получить фото (Испания)")
+
+        keyboard.add(button1)
+        keyboard.add(button2)
+        # Отправляем сообщение с клавиатурой
+        await message.answer(text="Бим бим бам бам", reply_markup=keyboard)
+
+    else:
+        await message.answer("Бот временно остановлен администратором")
+
+@dp.message_handler(commands=['count'])
+async def count(message: Message):
+    if on_off == "On" or admin(message.from_id):
+        with open(image_handler.backgrounds_list, 'r') as file:
+            # Прочитайте строки из файла и создайте массив ссылок
+            links = [line.strip() for line in file]
+
+        await message.answer(text=len(links))
+    else:
+        await message.answer("Бот временно остановлен администратором")
+
+@dp.message_handler(lambda message: message.text == 'Получить фото (Франция)')
+async def get_photo(message: Message):
+
+    
+    if on_off == "On" or admin(message.from_id):
+        if len(threads) > 10:
+
+            await message.answer("Большая нагрузка на бота, попробуйте позже!")
+        else:
+
+            threads.append("thread")
+            
+            images = image_handler.start_combine(country="FRANCE")
+
+            if images:
+
+                input_media_images = []
+                
+                i = 0
+
+                while i < len(images) - 1:
+                    image_stream = BytesIO()
+                    images[i].save(image_stream, format='JPEG')
+                    image_stream.seek(0)
+                    input_media_images.append(InputMediaPhoto(media=image_stream)) 
+                    i = i + 1   
+                print("Фото сгенерированы!")
+
+            await message.answer_media_group(media = input_media_images)
+            threads.pop()
+
+    else:
+        await message.answer("Бот временно остановлен администратором")
+    
+@dp.message_handler(lambda message: message.text == 'Получить фото (Испания)')
+async def get_photo(message: Message):
+
+    
+    if on_off == "On" or admin(message.from_id):
+        if len(threads) > 10:
+
+            await message.answer("Большая нагрузка на бота, попробуйте позже!")
+        else:
+
+            threads.append("thread")
+            
+            images = image_handler.start_combine(country="SPAIN")
+
+            if images:
+
+                input_media_images = []
+                
+                i = 0
+
+                while i < len(images) - 1:
+                    image_stream = BytesIO()
+                    images[i].save(image_stream, format='JPEG')
+                    image_stream.seek(0)
+                    input_media_images.append(InputMediaPhoto(media=image_stream)) 
+                    i = i + 1   
+                print("Фото сгенерированы!")
+
+            await message.answer_media_group(media = input_media_images)
+            threads.pop()
+
+    else:
+        await message.answer("Бот временно остановлен администратором")
+
+@dp.message_handler(commands=['help'])
+async def on_start(message: Message):
+
+    if admin(message.from_id):
+        await message.answer(text="stop - остановка бота\n"+
+                                    "start - запуск бота\n"
+                                    "add (id) - добавление админа\n"
+                                    "(текстовый документ) - добавление списка фонов\n"
+                                    "(группа фотографий) - изменение фото\n"
+                                    "/help - список функций админ-панели")
+
+# Обработчик приема документа
+@dp.message_handler(content_types=[types.ContentType.DOCUMENT])
+async def process_document(message: types.Message):
+    
+    try:
+        #добавление списка фонов
+        if admin(message.from_id):
+            on_off = "Off"
+            time.sleep(10)
+            # Скачиваем документ
+            file_path = await bot.get_file(message.document.file_id)
+            document = await bot.download_file(file_path.file_path)
+
+            # Извлекаем текст из загруженного документа
+            document_bytes = document.read()
+            new_backgrounds = document_bytes.decode("utf-8")      
+            new_backgrounds = new_backgrounds.replace("\r", "") 
+
+            with open(image_handler.backgrounds_list, "a") as file:
+                # Добавляем новый текст с новой строкой
+                file.write("\n"+ new_backgrounds)   
+
+            on_off = "On"
+            await message.answer(text="Фоны успешно добавлены!")
+    except:
+        await message.answer("Произошла ошибка")
+"""
+@dp.message_handler(content_types=[types.ContentType.PHOTO])
+async def process_photos(message: types.Message):
+
+    try:
+        if admin(message.from_id):
+
+            on_off = "Off"
+            #time.sleep(10)
+            #удаляем старые фото
+            image_handler.clear_overlays()
+
+            # Скачиваем и сохраняем новые фотографии
+            for i, photo in enumerate(message.photo):
+                file_id = photo.file_id
+                file_path = await bot.get_file(file_id)
+                file_name = f"{i+1}.jpg"  # Имя файла, например, "1.jpg", "2.jpg" и так далее
+                downloaded_photo = await bot.download_file(file_path.file_path)
+                with open(os.path.join(image_handler.overlay_folder, file_name), "wb") as file:
+                    file.write(downloaded_photo.read())
+
+            #on_off = "On"
+            await message.answer(text="Фото успешно изменены!")
+    except:
+        await message.answer("Произошла ошибка")
+"""
+@dp.message_handler()
+async def echo(message: Message):
+    global footage
+    try:
+        if admin(message.from_id):
+
+            #добавление администратора
+            if "add" in message.text:
+                admin_id = int(message.text.split(" ")[1])
+                admins.append(admin_id)
+                await message.answer(f"Добавлен администратор с id: {admin_id}")
+
+            #остановка бота
+            elif message.text == "stop":
+                if on_off == "On":
+                    on_off = "Off"
+                    await message.answer("Остановка бота")
+                else:
+                    await message.answer("Бот уже остановлен")
+
+            #запуск бота
+            elif message.text == "start":
+                if on_off == "Off":
+                    on_off = "On"
+                    await message.answer("Запуск бота")
+                else:
+                    await message.answer("Бот уже запущен")
+
+            #статус
+            elif message.text == "status":
+                
+                await message.answer(on_off)
+
+            return
+            
+    except: 
+        await message.answer(text="Попробуйте позже")
+
+
+def main():
+    # Запуск бота
+    executor.start_polling(dp, skip_updates=True)
+
+if __name__ == '__main__':
+    main()
---
+++ server/image_handler.py
@@ -0,0 +1,154 @@
+from PIL import Image
+import os
+import random
+from io import BytesIO
+import requests
+import pooling
+
+overlay_folder_FR = "overlay_FR"
+overlay_folder_SP = "overlay_SP"
+backgrounds_list = "backgrounds.txt"
+
+footage_folder = ""
+
+def clear_overlays():
+    # Получаем список файлов и подпапок в папке
+    files = os.listdir(overlay_folder)
+    for file in files:
+        if file != "a.jpg":
+            file_path = os.path.join(overlay_folder, file)
+            os.remove(file_path)
+
+def start_combine(country):
+
+    if country == "FRANCE":
+        overlay_folder = overlay_folder_FR
+    if country == "SPAIN":
+        overlay_folder = overlay_folder_SP
+    #получение спика фоток для наложения
+    overlay_dict = {}
+
+    for filename in os.listdir(overlay_folder):
+        # Проверяем, что файл имеет расширение изображения (например, .jpg или .png)
+        if filename.endswith((".jpg", ".png")):
+            # Формируем полный путь к файлу
+            file_path = os.path.join(overlay_folder, filename)
+            
+            # Загружаем изображение с помощью Pillow
+            image = Image.open(file_path)
+            
+            # Добавляем изображение в словарь, используя имя файла как ключ
+            overlay_dict[filename] = image
+
+    #получение рандомной ссылки на фото из списка ссылок фонов
+    with open(backgrounds_list, 'r') as file:
+        # Прочитайте строки из файла и создайте массив ссылок
+        links = [line.strip() for line in file]
+
+    random_link1 = random.choice(links)
+    random_link2 = random.choice(links)
+
+    #запрос на получение картинки
+    response1 = requests.get(random_link1)
+    response2 = requests.get(random_link2)
+    
+    background1 = Image.open(BytesIO(response1.content))
+    background2 = Image.open(BytesIO(response2.content))
+
+    
+    result_images = []
+    i = 1
+
+    for overlay in sorted(overlay_dict):
+        if i % 2 == 0:
+            image = combine(overlay_image=overlay_dict[overlay], background_image=background1, result_name=overlay)
+        else:
+            image = combine(overlay_image=overlay_dict[overlay], background_image=background2, result_name=overlay)
+        i = i + 1
+        result_images.append(image)
+    
+    background1.close()
+    background2.close()
+    image.close()   
+
+    # Удалите выбранный элемент из массива ссылок
+    links.remove(random_link1)
+    links.remove(random_link2)
+
+    # Откройте файл для записи и перезапишите его с обновленным списком ссылок
+    with open(backgrounds_list, 'w') as file:
+        for link in links:
+            file.write(link + '\n')
+
+    for image in overlay_dict:
+        overlay_dict[image].close()
+
+    return result_images
+
+    
+
+
+
+def combine(overlay_image, background_image, result_name):
+
+    # Получаем размеры изображения
+    width, height = background_image.size
+
+    # Вычисляем новые размеры и координаты для вырезки
+    new_width = min(width, height * 9 // 16)  # Ширина будет 9/16 от высоты
+    new_height = min(height, width * 16 // 9)  # Высота будет 16/9 от ширины
+    left = 0  # Начало вырезки по горизонтали
+    top = height - new_height  # Начало вырезки по вертикали (снизу)
+
+    # Вырезаем часть изображения
+    background_image = background_image.crop((left, top, left + new_width, top + new_height))
+
+    # Меняем размер до 1080x1920
+    background_image = background_image.resize((1080, 1920), Image.LANCZOS)
+
+    # Получаем размеры фона и наложения
+    background_width, background_height = background_image.size
+    overlay_width, overlay_height = overlay_image.size
+
+    # Рассчитываем новые размеры наложения, чтобы оно соответствовало разрешению 1080x1920
+    new_overlay_width = 900
+    new_overlay_height = int(overlay_height * (new_overlay_width / overlay_width))
+
+    # Масштабируем наложение к новым размерам
+    overlay_image = overlay_image.resize((new_overlay_width, new_overlay_height), Image.LANCZOS)
+
+    # Рассчитываем координаты, чтобы разместить наложение в центре фона с рамками
+    x = (background_width - new_overlay_width) // 2
+    y = (background_height - new_overlay_height) // 2
+
+    # Создаем новое изображение, накладывая фон и наложение
+    result = background_image.copy()
+    result.paste(overlay_image, (x, y))
+    """
+    footage = Image.open(f"{footage_folder}//footage{random.randint(1,7)}.png")
+
+    # Получаем размеры футажа и фона
+    overlay_width, overlay_height = footage.size
+    background_width, background_height = result.size
+
+    # Выбираем случайное положение для вставки футажа
+    x_position = random.randint(0, overlay_width - background_width)
+
+    # Вырезаем фрагмент из футажа
+    crop_box = (x_position, 0, x_position + background_width, background_height)
+    overlay_fragment = footage.crop(crop_box)
+
+    # Вставляем фрагмент футажа на фон
+    result.paste(overlay_fragment, (0,0), overlay_fragment)
+        
+    footage.close()
+    """
+    return result
+
+def main():
+    start_combine()
+
+
+
+if __name__ == "__main__":
+    main()
